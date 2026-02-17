f99e4b433e4c46f7e381c1d7cbe8935356df01a6
usman_coder_5 <muhammadusmansubhani12@gmail.com>
2026-02-17 13:31:38 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/blogs', (req,res) =>{
+    res.send('blog.html')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
