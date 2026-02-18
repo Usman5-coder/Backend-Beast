43007bd9cb8c735e0f7b3c899458ac0b4789f74d
usman_coder_5 <muhammadusmansubhani12@gmail.com>
2026-02-18 12:37:16 +0500

    First mongoose schema added

---
+++ Data modeling/models/todos/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password:{
+        type: Integar,
+        required:true,
+        min: [8, "Password must have at least 8 characters!"],
+        max: 12
+    }
+}, {timestamps: true})
+
+export const User = mongoose.model('User', userSchema)
